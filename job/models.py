9309bdff5b73d964bd30da49a3a62161324d488e
gamebad623 <gamebad623@gmail.com>
2023-12-07 22:52:54 +0200

    add job views & url

--- job/models.py
+++ job/models.py
@@ -6,13 +6,19 @@ JOB_TYPE = (
     ('Part Time','Part Time'),
 )
 class Job(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100 , )
     job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
     description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default= 1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-
+    category = models.ForeignKey('Category', on_delete= models.CASCADE , blank = True)
     def __str__(self):
         return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
